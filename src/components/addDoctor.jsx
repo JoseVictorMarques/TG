30c4f85c6c1035b40c1ae2d50780731122a1e51f
JoseVictorMarques <54557984+JoseVictorMarques@users.noreply.github.com>
2022-04-16 11:58:22 -0300

    wip authorization

--- src/components/addDoctor.jsx
+++ src/components/addDoctor.jsx
@@ -2,8 +2,8 @@ import { Button } from '@material-ui/core';
 
 
 export default function AddDoctor(props) {
-    
-    function addDoctor(name){
+
+    function addDoctor(name, specialty){
 
         try{
           props.contract.methods.addDoctor(name).send( {from: props.accounts[0]} )
@@ -13,6 +13,7 @@ export default function AddDoctor(props) {
           console.log(error);
         }
         document.getElementById("Doctor Name").value = '';
+        document.getElementById("Doctor Specialty").value = '';
       }
 
     return(
@@ -22,11 +23,15 @@ export default function AddDoctor(props) {
                         id="Doctor Name"
                         className="textinput"
                         placeholder="Doctor Name"/>
+                    <input 
+                        id="Doctor Specialty"
+                        className="textinput"
+                        placeholder="Doctor Specialty"/>
                 </div>
                 <Button 
                     variant="contained"
                     style={{backgroundColor: '#63235A', color: '#FFFFFF', float: 'right', marginRight:'220px', marginTop:'10px'}}
-                    onClick={(e)=>addDoctor(document.getElementById('Doctor Name').value)}
+                    onClick={(e)=>addDoctor(document.getElementById('Doctor Name').value, document.getElementById('Doctor Specialty').value)}
                 >
                     SUBMIT
                 </Button>
