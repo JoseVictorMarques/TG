64893673b16f6b66087c5b821ad91b7c8349c6fb
JoseVictorMarques <54557984+JoseVictorMarques@users.noreply.github.com>
2022-04-23 22:11:52 -0300

    ajustando tela de escolha

--- src/components/login/signIn.jsx
+++ src/components/login/signIn.jsx
@@ -5,7 +5,7 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { useState } from 'react';
 import { Redirect } from "react-router-dom";
-
+import './signIn.css'
 
 function SignIn() {
   const [actor, setActor] = useState('');
@@ -15,29 +15,30 @@ function SignIn() {
   };
 
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Occupation</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={actor}
-          label="Occupation"
-          onChange={handleChange}
-        >
-          <MenuItem value={1}>Doctor</MenuItem>
-          <MenuItem value={2}>Patient</MenuItem>
-          <MenuItem value={3}>Regulator</MenuItem>
-        </Select>
-      </FormControl>
-      {
-        actor === 1? <Redirect to="/doctor" />:
-        actor === 2? <Redirect to="/patient" />:
-        actor === 3? <Redirect to="/regulator" />:
-        <Redirect to="/" />
-      }
-    </Box>
-    
+      <div className='SignIn-All'>
+        <Box className='Box-Selector'>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Occupation</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={actor}
+                label="Occupation"
+                onChange={handleChange}
+              >
+                <MenuItem value={1}>Doctor</MenuItem>
+                <MenuItem value={2}>Patient</MenuItem>
+                <MenuItem value={3}>Regulator</MenuItem>
+              </Select>
+            </FormControl>
+            {
+              actor === 1? <Redirect to="/doctor" />:
+              actor === 2? <Redirect to="/patient" />:
+              actor === 3? <Redirect to="/regulator" />:
+              <Redirect to="/" />
+            }
+          </Box>
+      </div>
   );
 }
 
