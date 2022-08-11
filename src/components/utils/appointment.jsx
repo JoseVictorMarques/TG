c2e37ea87c895c91ee390462e725345452e90d6b
JoseVictorMarques <54557984+JoseVictorMarques@users.noreply.github.com>
2022-08-10 22:26:15 -0300

    adding obs in diagnosis

--- src/components/utils/appointment.jsx
+++ src/components/utils/appointment.jsx
@@ -3,7 +3,7 @@ import { Button } from '@material-ui/core';
 export default function Appointment(props) {
 
 
-    function addAppointment(patientID,diagnosis,medicine,exam){
+    function addAppointment(patientID,diagnosis,medicine,exam,obs){
 
         var d_id = parseInt(props.id);
         var p_id = parseInt(patientID);
@@ -18,7 +18,7 @@ export default function Appointment(props) {
             try{
                 var timestamp = new Date().getTime();
                 timestamp = parseInt(timestamp);
-                props.contract.methods.appointment(d_id,p_id,diagnosis,med_code,exam_code,timestamp).send( {from: props.accounts[0],gas:3000000} )
+                props.contract.methods.appointment(d_id,p_id,diagnosis,med_code,exam_code,timestamp,obs).send( {from: props.accounts[0],gas:3000000} )
             }catch(error){
                 console.log(error);
             }
@@ -27,6 +27,7 @@ export default function Appointment(props) {
         document.getElementById("Diagnosis").value = '';
         document.getElementById("Medicine").value = '';
         document.getElementById("Exam").value = '';
+        document.getElementById("Obs").value = '';
       }
 
     return(
@@ -50,17 +51,23 @@ export default function Appointment(props) {
                 placeholder="Exam"/>
             </div>
             <div>
-            <textarea 
+                <input
                 id="Diagnosis"
-                className="diagnosis_input"
+                className="textinput"
                 placeholder="Diagnosis"/>
+            </div>
+            <div>
+                <input
+                id="Obs"
+                className="textinput"
+                placeholder="Comments"/>
             </div>
             <Button 
                 variant="contained"
                 style={{backgroundColor: '#63235A', color: '#FFFFFF', float: 'right', marginRight:'220px'}}
                 onClick={(e)=>addAppointment(document.getElementById('Patient ID').value,
                 document.getElementById('Diagnosis').value, document.getElementById('Medicine').value,
-                document.getElementById('Exam').value)}>
+                document.getElementById('Exam').value,document.getElementById('Obs').value)}>
                 SUBMIT
             </Button>
         </div>
